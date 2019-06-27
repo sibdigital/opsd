0b1f73e7c9671850108f048992decddbe91ef7bc
zbd <bulzhamur@gmail.com>
2019-06-27 11:49:36 +0800

    Добавил роли (задача #96)

--- app/seeders/basic_data/role_seeder.rb
+++ app/seeders/basic_data/role_seeder.rb
@@ -156,5 +156,233 @@ module BasicData
         ]
       }
     end
+
+    def project_curator
+      { name: "Куратор проекта", position: 3, permissions: [
+        :view_work_packages,
+        :export_work_packages,
+        :add_work_packages,
+        :move_work_packages,
+        :edit_work_packages,
+        :add_work_package_notes,
+        :edit_own_work_package_notes,
+        :manage_work_package_relations,
+        :manage_subtasks,
+        :manage_public_queries,
+        :save_queries,
+        :view_work_package_watchers,
+        :add_work_package_watchers,
+        :delete_work_package_watchers,
+        :view_calendar,
+        :comment_news,
+        :manage_news,
+        :log_time,
+        :view_time_entries,
+        :view_own_time_entries,
+        :edit_own_time_entries,
+        :view_timelines,
+        :edit_timelines,
+        :delete_timelines,
+        :view_reportings,
+        :edit_reportings,
+        :delete_reportings,
+        :manage_wiki,
+        :manage_wiki_menu,
+        :rename_wiki_pages,
+        :change_wiki_parent_page,
+        :delete_wiki_pages,
+        :view_wiki_pages,
+        :export_wiki_pages,
+        :view_wiki_edits,
+        :edit_wiki_pages,
+        :delete_wiki_pages_attachments,
+        :protect_wiki_pages,
+        :list_attachments,
+        :add_messages,
+        :edit_own_messages,
+        :delete_own_messages,
+        :browse_repository,
+        :view_changesets,
+        :commit_access,
+        :view_commit_author_statistics,
+        :view_members
+      ]
+      }
+    end
+
+    def project_customer
+      { name: "Заказчик проекта", position: 3, permissions: [
+        :view_work_packages,
+        :export_work_packages,
+        :add_work_packages,
+        :move_work_packages,
+        :edit_work_packages,
+        :add_work_package_notes,
+        :edit_own_work_package_notes,
+        :manage_work_package_relations,
+        :manage_subtasks,
+        :manage_public_queries,
+        :save_queries,
+        :view_work_package_watchers,
+        :add_work_package_watchers,
+        :delete_work_package_watchers,
+        :view_calendar,
+        :comment_news,
+        :manage_news,
+        :log_time,
+        :view_time_entries,
+        :view_own_time_entries,
+        :edit_own_time_entries,
+        :view_timelines,
+        :edit_timelines,
+        :delete_timelines,
+        :view_reportings,
+        :edit_reportings,
+        :delete_reportings,
+        :manage_wiki,
+        :manage_wiki_menu,
+        :rename_wiki_pages,
+        :change_wiki_parent_page,
+        :delete_wiki_pages,
+        :view_wiki_pages,
+        :export_wiki_pages,
+        :view_wiki_edits,
+        :edit_wiki_pages,
+        :delete_wiki_pages_attachments,
+        :protect_wiki_pages,
+        :list_attachments,
+        :add_messages,
+        :edit_own_messages,
+        :delete_own_messages,
+        :browse_repository,
+        :view_changesets,
+        :commit_access,
+        :view_commit_author_statistics,
+        :view_members
+      ]
+      }
+    end
+
+    def project_office_manager
+      { name: "Руководитель Проектного офиса", position: 3, permissions: [
+        :view_work_packages,
+        :export_work_packages,
+        :add_work_packages,
+        :move_work_packages,
+        :edit_work_packages,
+        :add_work_package_notes,
+        :edit_own_work_package_notes,
+        :manage_work_package_relations,
+        :manage_subtasks,
+        :manage_public_queries,
+        :save_queries,
+        :view_work_package_watchers,
+        :add_work_package_watchers,
+        :delete_work_package_watchers,
+        :view_calendar,
+        :comment_news,
+        :manage_news,
+        :log_time,
+        :view_time_entries,
+        :view_own_time_entries,
+        :edit_own_time_entries,
+        :view_timelines,
+        :edit_timelines,
+        :delete_timelines,
+        :view_reportings,
+        :edit_reportings,
+        :delete_reportings,
+        :manage_wiki,
+        :manage_wiki_menu,
+        :rename_wiki_pages,
+        :change_wiki_parent_page,
+        :delete_wiki_pages,
+        :view_wiki_pages,
+        :export_wiki_pages,
+        :view_wiki_edits,
+        :edit_wiki_pages,
+        :delete_wiki_pages_attachments,
+        :protect_wiki_pages,
+        :list_attachments,
+        :add_messages,
+        :edit_own_messages,
+        :delete_own_messages,
+        :browse_repository,
+        :view_changesets,
+        :commit_access,
+        :view_commit_author_statistics,
+        :view_members
+      ]
+      }
+    end
+
+    def project_activity_coordinator
+      { name: "Координатор проектной деятельности", position: 3, permissions: [
+        :view_work_packages,
+        :export_work_packages,
+        :add_work_packages,
+        :move_work_packages,
+        :edit_work_packages,
+        :add_work_package_notes,
+        :edit_own_work_package_notes,
+        :manage_work_package_relations,
+        :manage_subtasks,
+        :manage_public_queries,
+        :save_queries,
+        :view_work_package_watchers,
+        :add_work_package_watchers,
+        :delete_work_package_watchers,
+        :view_calendar,
+        :comment_news,
+        :manage_news,
+        :log_time,
+        :view_time_entries,
+        :view_own_time_entries,
+        :edit_own_time_entries,
+        :view_timelines,
+        :edit_timelines,
+        :delete_timelines,
+        :view_reportings,
+        :edit_reportings,
+        :delete_reportings,
+        :manage_wiki,
+        :manage_wiki_menu,
+        :rename_wiki_pages,
+        :change_wiki_parent_page,
+        :delete_wiki_pages,
+        :view_wiki_pages,
+        :export_wiki_pages,
+        :view_wiki_edits,
+        :edit_wiki_pages,
+        :delete_wiki_pages_attachments,
+        :protect_wiki_pages,
+        :list_attachments,
+        :add_messages,
+        :edit_own_messages,
+        :delete_own_messages,
+        :browse_repository,
+        :view_changesets,
+        :commit_access,
+        :view_commit_author_statistics,
+        :view_members
+      ]
+      }
+    end
+
+    def project_office_coordinator
+      { name: "Координатор проекта от Проектного офиса", position: 5, permissions: Role.new.setable_permissions.map(&:name) }
+    end
+
+    def events_responsible
+      { name: "Ответственный за блок мероприятий", position: 5, permissions: Role.new.setable_permissions.map(&:name) }
+    end
+
+    def project_head
+      { name: "Руководитель проекта", position: 5, permissions: Role.new.setable_permissions.map(&:name) }
+    end
+
+    def project_office_admin
+      { name: "Администратор проектного офиса", position: 5, permissions: Role.new.setable_permissions.map(&:name) }
+    end
   end
 end
