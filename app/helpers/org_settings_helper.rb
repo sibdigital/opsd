4a1a850ec666da7dc43559b59673ce6660c00139
alxtmf <89021632777@mail.ru>
2019-07-02 20:21:31 +0800

    Должности

--- app/helpers/org_settings_helper.rb
+++ app/helpers/org_settings_helper.rb
@@ -11,7 +11,7 @@ module OrgSettingsHelper
       { name: 'iogv', partial: 'org_settings/iogv', label: :label_iogv },
       { name: 'municipalities', partial: 'org_settings/municipalities', label: :label_municipalities },
       { name: 'counterparties', partial: 'org_settings/counterparties', label: :label_counterparties },
-      { name: 'positions', partial: 'org_settings/counterparties', label: :label_counterparties }
+      { name: 'positions', partial: 'org_settings/positions', label: :label_positions }
     ]
   end
 
