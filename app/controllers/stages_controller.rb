fa2bef6268aca739a0c90504726b1143a47e121b
zbd <bulzhamur@gmail.com>
2019-07-03 17:17:34 +0800

    Заполнил вкладки этапов в соответствии с #113 Подкорректировал запрос по задаче #109. Добавил условие что пакет работ не завершен на 100%

--- app/controllers/stages_controller.rb
+++ app/controllers/stages_controller.rb
@@ -1,14 +1,68 @@
 class StagesController < ApplicationController
   menu_item :stages
-  before_action :find_optional_project
+
+  before_action :find_project
+  before_action :authorize
+  before_action :set_current_user
+  before_action :check_valid_tab
+  before_action :get_tab_stages
+
   include StagesHelper
 
   def index
-    @tab = params[:tab] || 'StageInitCustomField'
+    @tab = params[:tab] || 'StageInit'
+    #@altered_project = @project
+  end
+
+  def stages_init
+    @altered_project = @project
+
+    #@altered_project = Project.find(@project.id)
+
+    #@altered_project.attributes = permitted_params.project
+    # if validate_parent_id && @altered_project.save
+    #   if params['project'].has_key?('parent_id')
+    #     @altered_project.set_allowed_parent!(params['project']['parent_id'])
+    #   end
+    #   flash[:notice] = l(:notice_successful_update)
+    #   OpenProject::Notifications.send('project_updated', project: @altered_project)
+    # end
+  end
+
+  def stages_control
+    @remaining_days = Setting.remaining_count_days.to_i
+    now = Date.today + @remaining_days
+
+    if !@user.nil?
+      @works = []
+      @exp_works = []
+      if (MemberRole.joins("INNER JOIN members ON member_roles.member_id=members.id INNER JOIN roles ON member_roles.role_id=roles.id")
+            .where("position in (?) and user_id = ?", [6, 7, 9, 10, 11, 12, 14, 15, 16], @user.id)
+            .count > 0) or (@user.admin)
+        @works = WorkPackage.visible(@user).where("due_date >= :d1 and due_date <= :d2 and done_ratio<100",{d1: Date.today, d2: now})
+        @exp_works = WorkPackage.visible(@user).where('(due_date < ?) and (done_ratio<100)', Date.today)
+      else
+        @works = WorkPackage.with_assigned(@user).where("due_date >= :d1 and due_date <= :d2 and done_ratio<100",{d1: Date.today, d2: now})
+        @exp_works = WorkPackage.with_assigned(@user).where('(due_date < ?) and (done_ratio<100)', Date.today)
+      end
+    end
 
   end
 
   protected
+
+  def validate_parent_id
+    return true if User.current.admin?
+    parent_id = permitted_params.project && params[:project][:parent_id]
+    if parent_id || @project.new_record?
+      parent = parent_id.blank? ? nil : Project.find_by(id: parent_id.to_i)
+      unless @project.allowed_parents.include?(parent)
+        @project.errors.add :parent_id, :invalid
+        return false
+      end
+    end
+    true
+  end
 
   def default_breadcrumb
     if action_name == 'index'
@@ -19,15 +73,40 @@ class StagesController < ApplicationController
   end
 
   def show_local_breadcrumb
-    true
+    false
   end
 
-  def find_optional_project
-    return true unless params[:project_id]
+  def check_valid_tab
+    @selected_tab =
+      if params[:tab]
+        helpers.stages_tabs.detect { |t| t[:name] == params[:tab] }
+      else
+        helpers.stages_tabs.first
+      end
+
+    unless @selected_tab
+      render_404
+    end
+  end
+
+  def find_project
     @project = Project.find(params[:project_id])
     authorize
   rescue ActiveRecord::RecordNotFound
     render_404
+  end
+
+  ##
+  # Only load the needed elements for the current tab
+  def get_tab_stages
+    callback = "stages_#{@selected_tab[:name]}"
+    if respond_to?(callback, true)
+      send(callback)
+    end
+  end
+
+  def set_current_user
+    @user = current_user
   end
 
 end
--- app/helpers/stages_helper.rb
+++ app/helpers/stages_helper.rb
@@ -4,12 +4,37 @@ module StagesHelper
 
   def stages_tabs
     [
-      { name: 'StageInitCustomField', partial: 'stages/tab', label: :label_stage_init },
-      { name: 'StageAnalysisCustomField', partial: 'stages/tab', label: :label_stage_analysis },
-      { name: 'StagePlanningCustomField', partial: 'stages/tab', label: :label_stage_planning },
-      { name: 'StageExecutionCustomField', partial: 'stages/tab', label: :label_stage_execution },
-      { name: 'StageControlCustomField', partial: 'stages/tab', label: :label_stage_control },
-      { name: 'StageCompletionCustomField', partial: 'stages/tab', label: :label_stage_completion }
+      {
+        name: 'init',
+        action: :edit_project,
+        partial: 'stages/init',
+        label: :label_stage_init
+      },
+      {
+        name: 'analysis',
+        partial: 'stages/analysis',
+        label: :label_stage_analysis
+      },
+      {
+        name: 'planning',
+        partial: 'stages/planning',
+        label: :label_stage_planning
+      },
+      {
+        name: 'execution',
+        partial: 'stages/execution',
+        label: :label_stage_execution
+      },
+      {
+        name: 'control',
+        partial: 'stages/control',
+        label: :label_stage_control
+      },
+      {
+        name: 'completion',
+        partial: 'stages/completion',
+        label: :label_stage_completion
+      }
     ]
   end
 
