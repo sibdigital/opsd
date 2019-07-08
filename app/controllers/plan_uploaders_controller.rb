5fa991a22f4977d743d4f765080a5b974568e3a5
zbd <bulzhamur@gmail.com>
2019-07-09 03:11:55 +0800

    Загрузка xls выгрузки из эл.бюджета

--- app/controllers/plan_uploaders_controller.rb
+++ app/controllers/plan_uploaders_controller.rb
@@ -6,14 +6,21 @@ require 'roo'
 class PlanUploadersController < ApplicationController
 
   #include EBHelper
+  require 'date'
+
+  RUSSIAN_MONTH_NAMES_SUBSTITUTION = { 'январь' => 'jan', 'февраль' => 'feb', 'март' => 'mar',
+                                       'апрель' => 'apr', 'май' => 'may', 'июнь' => 'jun',
+                                       'июль' => 'jul', 'август' => 'aug', 'сентябрь' => 'sep',
+                                       'октябрь' => 'oct', 'ноябрь' => 'nov', 'декабрь' => 'dec'}
 
   def index
     @plan_uploaders = PlanUploader.all
   end
 
   def new
+    #@project = Project.find_by(identifier: params[:project_id])
+    @project = Project.find(params[:project_id])
     @plan_uploader = PlanUploader.new
-    @project = Project.find_by(identifier: params[:project_id])
   end
 
   def create
@@ -38,40 +45,62 @@ class PlanUploadersController < ApplicationController
 
   def load
     prepare_roo
-    filename = Rails.root.join('public',@plan_uploader.name.store_path)
+    filename = Rails.root.join('public', @plan_uploader.name.store_path)
     puts File.file?(filename)
     xlsx = Roo::Excelx.new(filename)
     ebrows = []
-    xlsx.each_row_streaming do |row|
-      r = EBRow.new row[1].value, row[2].value, row[3].value, row[4].value, row[5].value, row[6].value, row[7].value
+    xlsx.each_row_streaming(offset: 1) do |row|
+      r = EBRow.new row[0].value, row[3].value, russian_to_english_date(row[5].value), russian_to_english_date(row[6].value), nil, row[2].value, row[7].value
       ebrows.push r
     end
 
-    @project_for_load = Project.find_by(identifier: params[:project_id])
+    #@project_for_load = Project.find_by(identifier: params[:project_id])
+    @project_for_load = Project.find(params[:project_id])
 
     ebrows.each do |erow|
       puts erow.to_s
       if !erow.is_empty?
-        wp_name = "#{erow.serial_number} #{erow.name.to_s}"
+        #wp_name = "#{erow.serial_number} #{erow.name.to_s}"
+        wp_name = "#{erow.name.to_s}"
         wp_list = WorkPackage.where(subject: wp_name).to_a
         if wp_list.size == 0
           wp = WorkPackage.new
           params = {}
-          params['subject'] = wp_name[0,250]
+          params['subject'] = wp_name[0, 250]
           params['description'] = wp_name
           params['project_id'] = @project_for_load.id
+          params['start_date'] = DateTime.parse(erow.date_begin)
+          params['due_date'] = DateTime.parse(erow.date_end)
           params['type_id'] = Type.find_by(name: I18n.t(:default_type_task)).id
-          params['status_id'] = Status.default.id# find_by(name: I18n.t(:default_status_new))
+          params['status_id'] = Status.default.id # find_by(name: I18n.t(:default_status_new))
           params['plan_type'] = 'execution'
           params['author_id'] = User.current.id
           params['position'] = 1
           params['priority_id'] = IssuePriority.default.id
-          params['due_date'] = erow.date_end
           #params['assigned_to'] = 1
           wp.attributes = params
           wp.save!
+
+          erow.wp_id = WorkPackage.last.id
         else
 
+        end
+      end
+    end
+
+    ebrows.each do |erow|
+      if erow.wp_id != nil
+        if erow.control_level != nil
+          params = {}
+          params['from_id'] = ebrows[erow.control_level.to_i - 1].wp_id
+          params['to_id'] = erow.wp_id
+          params['hierarchy'] = 1
+          print params
+          if params['from_id'] != params['to_id']
+            re = Relation.new
+            re.attributes = params
+            re.save!
+          end
         end
       end
     end
@@ -95,7 +124,7 @@ class PlanUploadersController < ApplicationController
 
   class EBRow
 
-    attr_accessor :serial_number, :name, :date_begin, :date_end, :employee, :document_charact, :control_level
+    attr_accessor :serial_number, :name, :date_begin, :date_end, :employee, :document_charact, :control_level, :wp_id
 
     @serial_number
     @name
@@ -104,6 +133,7 @@ class PlanUploadersController < ApplicationController
     @employee
     @document_charact
     @control_level
+    @wp_id
 
     def initialize(serial_number, name, date_begin, date_end, employee, document_charact, control_level)
       @serial_number = serial_number
@@ -113,6 +143,7 @@ class PlanUploadersController < ApplicationController
       @employee = employee
       @document_charact = document_charact
       @control_level = control_level
+      @wp_id = nil
     end
 
     def to_s
@@ -122,10 +153,16 @@ class PlanUploadersController < ApplicationController
     def is_empty?
       (@serial_number == nil || @serial_number == 0  || @serial_number == '') && (@name == nil || @name == 0  || @name == '')
     end
+
   end
 
-  # private
+  private
   # def plan_uploader_params
   #   params.require(:plan_uploader).permit(:name, :attachment, :status, :upload_at)
   # end
+
+  def russian_to_english_date(date_string)
+    date_string.downcase.gsub(/январь|февраль|март|апрель|май|июнь|июль|август|сентябрь|октябрь|ноябрь|декабрь/, RUSSIAN_MONTH_NAMES_SUBSTITUTION)
+  end
+
 end
