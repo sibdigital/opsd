c4d47fa645e801e3beb8f969bce56e8892508bd9
tmd <tmd979@gmail.com>
2019-07-12 14:56:23 +0800

    Добавлены настраиваемые поля для организаций

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -5,20 +5,6 @@ class Organization < ActiveRecord::Base
 
     acts_as_customizable
 
-    has_and_belongs_to_many :custom_fields, -> {
-      order("#{CustomField.table_name}.position")
-    },
-                            class_name: 'OrganizationCustomField',
-                            join_table: "#{table_name_prefix}organizations#{table_name_suffix}",
-                            association_foreign_key: 'id'
-
-
-    # has_and_belongs_to_many :work_package_custom_fields, -> {
-    #   order("#{CustomField.table_name}.position")
-    # }, class_name: 'WorkPackageCustomField',
-    #                         join_table: "#{table_name_prefix}custom_fields_projects#{table_name_suffix}",
-    #                         association_foreign_key: 'custom_field_id'
-
     validates :name, uniqueness: true
     # validates_uniqueness_of :inn, conditions: -> { where.not(is_legal_entity: 0) } # позволяет создавать сколько угодно подразделений с одинаковым инн, пока нет юридического лица с таким инн
     validates :inn, :uniqueness => { :scope => :is_legal_entity }, :if => :is_legal_entity # позволяет создавать сколько угодно подразделений с одинаковым инн, но одно юридическое лицо на один инн
@@ -31,7 +17,7 @@ class Organization < ActiveRecord::Base
       name
     end
 
-  def all_fields
-    true
-  end
+    def all_fields
+      true
+    end
 end
