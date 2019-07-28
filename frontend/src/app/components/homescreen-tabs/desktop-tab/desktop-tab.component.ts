8537b3e21d9a7f9503aade2a6db1a7196d682280
bulat <bula3-1992@yandex.ru>
2019-07-28 17:25:24 +0800

    Красная и зеленая таблица главного стола

--- frontend/src/app/components/homescreen-tabs/desktop-tab/desktop-tab.component.ts
+++ frontend/src/app/components/homescreen-tabs/desktop-tab/desktop-tab.component.ts
@@ -15,7 +15,7 @@ import {WorkPackageResource} from "core-app/modules/hal/resources/work-package-r
 })
 export class DesktopTabComponent implements OnInit {
   protected readonly appBasePath:string;
-  public upcoming_tasks_count:any[] = [];
+  public data:any[] = [];
 
   constructor(
     protected halResourceService:HalResourceService,
@@ -24,7 +24,7 @@ export class DesktopTabComponent implements OnInit {
   }
 
   ngOnInit() {
-    const filters = [
+    const filtersGreen = [
       {
         status: {
           operator: 'o',
@@ -51,10 +51,11 @@ export class DesktopTabComponent implements OnInit {
       }
     ];
     this.halResourceService
-      .get<CollectionResource<WorkPackageResource>>(this.pathHelper.api.v3.work_packages.toString(), {filters: JSON.stringify(filters)})
+      .get<CollectionResource<WorkPackageResource>>(this.pathHelper.api.v3.work_packages.toString(), {filters: JSON.stringify(filtersGreen)})
       .toPromise()
       .then((resources:CollectionResource<WorkPackageResource>) => {
-        resources.elements.map( el => {
+        resources.elements.map( (el, i) => {
+          let row = this.data[i] ? this.data[i] :{};
           let id = el.id;
           let subject = el.subject;
           let projectId = '';
@@ -63,7 +64,54 @@ export class DesktopTabComponent implements OnInit {
             projectId = el.$links.project.href.substr(17);
             project = el.$links.project.title;
           }
-          this.upcoming_tasks_count.push({id: id, subject: subject, project: project, projectId: projectId});
+          let upcoming_tasks = {id: id, subject: subject, project: project, projectId: projectId};
+          row["upcoming_tasks"] = upcoming_tasks;
+          this.data[i] = row;
+        });
+      });
+    const filtersRed = [
+      {
+        status: {
+          operator: 'o',
+          values: []
+        },
+      },
+      {
+        planType: {
+          operator: '~',
+          values: ['execution']
+        }
+      },
+      {
+        type: {
+          operator: '=',
+          values: ['2']
+        }
+      },
+      {
+        dueDate: {
+          operator: '<t-',
+          values: ['0']
+        }
+      }
+    ];
+    this.halResourceService
+      .get<CollectionResource<WorkPackageResource>>(this.pathHelper.api.v3.work_packages.toString(), {filters: JSON.stringify(filtersRed)})
+      .toPromise()
+      .then((resources:CollectionResource<WorkPackageResource>) => {
+        resources.elements.map( (el, i) => {
+          let row = this.data[i] ? this.data[i] :{};
+          let id = el.id;
+          let subject = el.subject;
+          let projectId = '';
+          let project = '';
+          if (el.$links.project) {
+            projectId = el.$links.project.href.substr(17);
+            project = el.$links.project.title;
+          }
+          let due_milestone = {id: id, subject: subject, project: project, projectId: projectId};
+          row["due_milestone"] = due_milestone;
+          this.data[i] = row;
         });
       });
   }
@@ -73,6 +121,30 @@ export class DesktopTabComponent implements OnInit {
       return "colored-col-bright-green";
     } else {
       return "colored-col-green";
+    }
+  }
+
+  public getRedClass(i:number):string {
+    if (i % 2 === 0) {
+      return "colored-col-bright-red";
+    } else {
+      return "colored-col-red";
+    }
+  }
+
+  public getOrangeClass(i:number):string {
+    if (i % 2 === 0) {
+      return "colored-col-bright-orange";
+    } else {
+      return "colored-col-orange";
+    }
+  }
+
+  public getPurpleClass(i:number):string {
+    if (i % 2 === 0) {
+      return "colored-col-bright-purple";
+    } else {
+      return "colored-col-purple";
     }
   }
 
