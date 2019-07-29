abfb280fbdd96d9001a61ddb05276210ad42556f
alxtmf <89021632777@mail.ru>
2019-07-29 20:02:50 +0800

    добавлены единицы измерения

--- app/models/target.rb
+++ app/models/target.rb
@@ -8,6 +8,7 @@ class Target < ActiveRecord::Base
   #)
   #tan(
   has_many :work_package_targets, foreign_key: 'target_id'
+  belongs_to :measure_unit, foreign_key: 'measure_unit_id'
   # )
 
   def option_name
--- app/seeders/basic_data_seeder.rb
+++ app/seeders/basic_data_seeder.rb
@@ -50,6 +50,7 @@ class BasicDataSeeder < CompositeSeeder
       # #tan(
       BasicData::ProjectStatusSeeder,
       BasicData::ProjectApproveStatusSeeder,
+      BasicData::MeasureUnitSeeder,
       # )
       BasicData::SettingSeeder
     ]
