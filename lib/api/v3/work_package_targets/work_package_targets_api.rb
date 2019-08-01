77995650b24054b1c2a458d4dd898119cfb058f3
bulat <bula3-1992@yandex.ru>
2019-08-01 10:39:26 +0800

    Вкладка Показатели на SQL

--- lib/api/v3/work_package_targets/work_package_targets_api.rb
+++ lib/api/v3/work_package_targets/work_package_targets_api.rb
@@ -36,6 +36,56 @@ module API
             end
           end
         end
+
+        resources :work_package_targets_1c do
+          before do
+            sql = "with slice as (select id, project_id, target_id, work_package_id, year, quarter, max(month) as month
+from work_package_targets as wpt
+group by id, project_id, target_id, work_package_id, year, quarter
+),
+slice_values as (
+   select s.*, value, plan_value
+   from slice as s
+            inner join work_package_targets as w
+                       on (s.project_id, s.target_id, s.work_package_id, s.year, s.quarter, s.month) =
+                          (w.project_id, w.target_id, w.work_package_id, w.year, w.quarter, w.month)
+)
+select id, project_id, target_id, work_package_id, year, plan_value, value, 1 as quarter1, 0 as quarter2, 0 as quarter3, 0 as quarter4
+FROM slice_values as wpt
+where quarter = 1 and year = date_part('year', CURRENT_DATE)
+union all
+select id, project_id, target_id, work_package_id, year, plan_value, value, 0 as quarter1, 2 as quarter2, 0 as quarter3, 0 as quarter4
+FROM slice_values as wpt
+where quarter = 2 and year = date_part('year', CURRENT_DATE)
+union all
+select id, project_id, target_id, work_package_id, year, plan_value, value, 0 as quarter1, 0 as quarter2, 3 as quarter3, 0 as quarter4
+FROM slice_values as wpt
+where quarter = 3 and year = date_part('year', CURRENT_DATE)
+union all
+select id, project_id, target_id, work_package_id, year, plan_value, value, 0 as quarter1, 0 as quarter2, 0 as quarter3, 4 as quarter4
+FROM slice_values as wpt
+where quarter = 4 and year = date_part('year', CURRENT_DATE)
+"
+            @work_package_targets = []
+            ActiveRecord::Base.connection.execute(sql).each { |wpt|
+              @work_package_target = Hash.new
+              @work_package_target['target'] = Target.find(wpt['target_id']).name
+              @work_package_target['projectId'] = wpt['project_id']
+              @work_package_target['name'] = WorkPackage.find(wpt['work_package_id']).subject
+              @work_package_target['otvetstvenniy'] = Project.find(wpt['project_id']).curator
+              @work_package_target['_type'] = 'Target'
+              @work_package_target['quarter1'] = wpt['quarter1']
+              @work_package_target['quarter2'] = wpt['quarter2']
+              @work_package_target['quarter3'] = wpt['quarter3']
+              @work_package_target['quarter4'] = wpt['quarter4']
+              @work_package_targets << @work_package_target
+            }
+          end
+
+          get do
+            @work_package_targets
+          end
+        end
       end
     end
   end
