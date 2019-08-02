ca4e4ab42a28d3bd8cc5a75f0a5d3efa1c3178d0
bulat <bula3-1992@yandex.ru>
2019-08-02 11:08:05 +0800

    Диаграмма ДЕСКТОП-КТ

--- lib/api/v3/diagrams/diagram_homescreen_representer.rb
+++ lib/api/v3/diagrams/diagram_homescreen_representer.rb
@@ -24,7 +24,7 @@ module API
                    when 'fed_budget' then fed_budget_data
                    when 'reg_budget' then reg_budget_data
                    when 'other_budget' then other_budget_data
-                   else [0,0,0,0]
+                   else [0, 0, 0, 0]
                    end
                  },
                  render_nil: true
@@ -41,6 +41,7 @@ module API
         end
 
         private
+
         # bbm(
         # Функция заполнения значений долей диаграммы Показатели на рабочем столе
         # В случае имзенения алгоритма среднего процента исполнения переделать функцию completed_percent_sd в project.rb
@@ -67,23 +68,76 @@ module API
 
         # Функция заполнения значений долей диаграммы KT на рабочем столе
         def desktop_kt_data
-          ispolneno = 0 # Порядок важен
-          ne_ispolneno = 0
+          ispolneno = 0
           v_rabote = 0
+          ne_ispolneno = 0
           riski = 0
-          WorkPackage
-            .where(type_id: 2, plan_type: :execution)
-            .map { |f| [f.status.is_closed, f.due_date] }
-            .each do |wp|
-            # TODO: Доделать как разбить WP по группам согласно ТЗ
-            if wp[0] = true
+          status_isp = Status.find_by(name: I18n.t(:default_status_completed))
+          WorkPackage # С закрытыми рисками
+            .joins(:type)
+            .joins(:work_package_problems)
+            .where("status_id = ?", status_isp.id)
+            .where(types: { name: I18n.t(:default_type_milestone) },
+                   plan_type: :execution, result_agreed: true, done_ratio: 100)
+            .select(:id, "min(work_package_problems.status)")
+            .group(:id)
+            .map do |wp|
+            if wp.min == 'solved'
               ispolneno += 1
-            elsif wp[1] > Time.zone.now.beginning_of_day
+            end
+          end
+          WorkPackage # То же самое но вообще без рисков
+            .joins(:type)
+            .where("status_id = ?", status_isp.id)
+            .where(types: { name: I18n.t(:default_type_milestone) },
+                   plan_type: :execution, result_agreed: true, done_ratio: 100)
+            .map do |wp|
+            if wp.work_package_problems.empty?
+              ispolneno += 1
+            end
+          end
+          status_vrab = Status.find_by(name: I18n.t(:default_status_in_work))
+          WorkPackage
+            .joins(:type)
+            .joins(:work_package_problems)
+            .where("status_id = ? and due_date > NOW()", status_vrab.id)
+            .where(types: { name: I18n.t(:default_type_milestone) }, plan_type: :execution)
+            .select(:id, "min(work_package_problems.status)")
+            .group(:id)
+            .map do |wp|
+            if wp.min == 'solved'
               v_rabote += 1
-            else
+            end
+          end
+          WorkPackage
+            .joins(:type)
+            .where("status_id = ? and due_date > NOW()", status_vrab.id)
+            .where(types: { name: I18n.t(:default_type_milestone) }, plan_type: :execution)
+            .map do |wp|
+            if wp.work_package_problems.empty?
+              v_rabote += 1
+            end
+          end
+          status_otm = Status.find_by(name: I18n.t(:default_status_cancelled))
+          WorkPackage
+            .joins(:type)
+            .where("due_date < NOW()", status_vrab.id)
+            .where(types: { name: I18n.t(:default_type_milestone) }, plan_type: :execution)
+            .map do |wp|
+            if wp.done_ratio != 100 or (wp.status.name != status_otm.name and wp.status.name != status_isp.name)
               ne_ispolneno += 1
             end
           end
+          WorkPackage
+            .joins(:type)
+            .joins(:work_package_problems)
+            .where("status_id not in (?, ?) and due_date > NOW()", status_otm.id, status_isp.id)
+            .where(types: { name: I18n.t(:default_type_milestone) }, plan_type: :execution)
+            .select(:id, "min(work_package_problems.status)")
+            .group(:id)
+            .map do |wp|
+            riski += 1
+            end
           result = []
           # Порядок важен
           result << ispolneno
@@ -108,8 +162,8 @@ module API
         # Функция заполнения значений долей диаграммы Бюджет на рабочем столе
         def fed_budget_data
           cost_objects = CostObject.all
-          total_budget = BigDecimal.new("0")
-          spent = BigDecimal.new("0")
+          total_budget = BigDecimal("0")
+          spent = BigDecimal("0")
 
           cost_objects.each do |cost_object|
             total_budget += cost_object.budget
@@ -128,9 +182,9 @@ module API
 
         def reg_budget_data
           cost_objects = CostObject.all
-          total_budget = BigDecimal.new("0")
-          labor_budget = BigDecimal.new("0")
-          spent = BigDecimal.new("0")
+          total_budget = BigDecimal("0")
+          labor_budget = BigDecimal("0")
+          spent = BigDecimal("0")
 
           cost_objects.each do |cost_object|
             total_budget += cost_object.budget
@@ -150,9 +204,9 @@ module API
 
         def other_budget_data
           cost_objects = CostObject.all
-          total_budget = BigDecimal.new("0")
-          material_budget = BigDecimal.new("0")
-          spent = BigDecimal.new("0")
+          total_budget = BigDecimal("0")
+          material_budget = BigDecimal("0")
+          spent = BigDecimal("0")
 
           cost_objects.each do |cost_object|
             total_budget += cost_object.budget
@@ -170,17 +224,16 @@ module API
           result << ostatok
         end
 
-
         def indicator_zdravoohranenie_data
-          [0,0,0,0]
+          [0, 0, 0, 0]
         end
 
         def indicator_obrazovanie_data
-          [0,0,0,0]
+          [0, 0, 0, 0]
         end
 
         def indicator_proizvoditelnost_truda_data
-          [0,0,0,0]
+          [0, 0, 0, 0]
         end
       end
     end
