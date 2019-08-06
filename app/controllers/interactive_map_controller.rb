eb8e7eb6eb84652e23ffa94525b6fec67811c33f
griboedok <griboedok@gmail.com>
2019-08-06 13:10:15 +0800

    Interactive Map:  - added seeders to work_package  - reworked query for data

--- app/controllers/interactive_map_controller.rb
+++ app/controllers/interactive_map_controller.rb
@@ -6,7 +6,7 @@ class InteractiveMapController < ApplicationController
   end
   def get_dues
 
-    cp=WorkPackage.where(project_id: params["project_id"], plan_type: :execution)
+    cp=WorkPackage.where(project_id: params["project_id"], plan_type: :execution).where("raion_id > ?", 0)
          .map { |f| [f.subject, f.status.is_closed, f.due_date, f.id, f.assigned_to.firstname, f.assigned_to.lastname, f.raion_id]} #region_id
     cp.each do |f|
       # f={name => f.subject, status => f.status.is_closed, due_date => f.due_date, risk => WorkPackageProblem.where(work_package_id: f.id).map {|p| [p.risk_id]}}
