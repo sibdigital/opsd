7c6276f35627e4ca0051d96b8c4917b447e5da75
alxtmf <89021632777@mail.ru>
2019-08-09 15:23:17 +0800

    Исправлен фильтр организации на случай, если организация пользователя пустая

--- app/models/queries/work_packages/filter/organization_filter.rb
+++ app/models/queries/work_packages/filter/organization_filter.rb
@@ -4,7 +4,7 @@ class Queries::WorkPackages::Filter::OrganizationFilter < ::Queries::WorkPackage
   #self.model = Organization
 
   def type
-    :integer
+    :list_optional#:integer
   end
 
   def human_name
@@ -16,8 +16,24 @@ class Queries::WorkPackages::Filter::OrganizationFilter < ::Queries::WorkPackage
   end
 
   def allowed_values
-    values || []
+    # o = Organization.all.map{ |r| [r.name, r.id.to_s]  }
+    # oa = o.to_a
+    # oa
+    org = User.current.organization
+    if org != nil
+      childs = org.childs().map { |r| [r.name, r.id.to_s] }
+    else
+      childs = [['', 0]]
+    end
+    childs
+    #childs = org != nil ? org.childs()#.map {|c| c.id.to_i} : [0]
+    #Organization.all { |r| [r.name, r.id.to_s]  }.to_a#values || []
   end
+
+  # def value_objects
+  #   int_values = values.map(&:to_i)
+  #   int_values
+  # end
 
   # def value_objects
   #   values || []
@@ -31,9 +47,9 @@ class Queries::WorkPackages::Filter::OrganizationFilter < ::Queries::WorkPackage
     true
   end
 
-  def where
-    operator_strategy.sql_for_field(values, self.class.model.table_name, self.class.key)
-  end
+  # def where
+  #   operator_strategy.sql_for_field(values, self.class.model.table_name, self.class.key)
+  # end
 
   ##
   # def self.key
--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
-  config.lograge.enabled = Rails.env.production? || ENV['OPENPROJECT_LOGRAGE_ENABLED']
+  config.lograge.enabled = true#Rails.env.production? || ENV['OPENPROJECT_LOGRAGE_ENABLED']
   config.lograge.base_controller_class = %w[ActionController::Base]
+
+  config.lograge.ignore_actions = ['AlertsController#get_pop_up_alerts']
 
   # Add custom data to event payload
   config.lograge.custom_payload do |controller|
