c02956a48a443442fc0c96c972e0ad910faaf5c6
alxtmf <89021632777@mail.ru>
2019-08-10 15:33:56 +0800

    Исправлены представления

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.lograge.enabled = true#Rails.env.production? || ENV['OPENPROJECT_LOGRAGE_ENABLED']
+  config.lograge.enabled = Rails.env.production? || ENV['OPENPROJECT_LOGRAGE_ENABLED']
   config.lograge.base_controller_class = %w[ActionController::Base]
 
   config.lograge.ignore_actions = ['AlertsController#get_pop_up_alerts']
