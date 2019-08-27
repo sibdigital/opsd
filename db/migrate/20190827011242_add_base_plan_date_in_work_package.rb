437706be2f3ce6dbb89ee81b749cd9074a6b57cb
alxtmf <89021632777@mail.ru>
2019-08-27 20:11:19 +0800

    Создана таблица задач пользователя.

--- db/migrate/20190827011242_add_base_plan_date_in_work_package.rb
+++ db/migrate/20190827011242_add_base_plan_date_in_work_package.rb
@@ -5,9 +5,9 @@ class AddBasePlanDateInWorkPackage < ActiveRecord::Migration[5.2]
     add_column :work_packages, :first_start_date, :timestamp
     add_column :work_packages, :last_start_date, :timestamp
 
-    add_column :work_package_journals, :first_due_date, :timestamp
-    add_column :work_package_journals, :last_due_date, :timestamp
-    add_column :work_package_journals, :first_start_date, :timestamp
-    add_column :work_package_journals, :last_start_date, :timestamp
+    add_column :work_package_journals, :first_due_date, :date
+    add_column :work_package_journals, :last_due_date, :date
+    add_column :work_package_journals, :first_start_date, :date
+    add_column :work_package_journals, :last_start_date, :date
   end
 end
