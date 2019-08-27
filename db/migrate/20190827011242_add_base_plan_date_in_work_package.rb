71d07bd507cd9ec524fcdaefda1e3fc018d3f8ff
alxtmf <89021632777@mail.ru>
2019-08-27 11:42:21 +0800

    В рабочий пакет добавлена автоматическая установка первых дат начала и завершения пакета работ и последних дат начала и завершения пакета работ. Требуется для диаграммы ганта и сравнения с базовым планом.

---
+++ db/migrate/20190827011242_add_base_plan_date_in_work_package.rb
@@ -0,0 +1,13 @@
+class AddBasePlanDateInWorkPackage < ActiveRecord::Migration[5.2]
+  def change
+    add_column :work_packages, :first_due_date, :timestamp
+    add_column :work_packages, :last_due_date, :timestamp
+    add_column :work_packages, :first_start_date, :timestamp
+    add_column :work_packages, :last_start_date, :timestamp
+
+    add_column :work_package_journals, :first_due_date, :timestamp
+    add_column :work_package_journals, :last_due_date, :timestamp
+    add_column :work_package_journals, :first_start_date, :timestamp
+    add_column :work_package_journals, :last_start_date, :timestamp
+  end
+end
