12986dc0d4a3d1d405cba3ae1863da7dd41870b8
alxtmf <89021632777@mail.ru>
2019-09-17 10:38:18 +0800

    Исправление бюджетов

--- config/initializers/menus.rb
+++ config/initializers/menus.rb
@@ -317,7 +317,7 @@ Redmine::MenuManager.map :admin_menu do |menu|
   menu.push :additional_settings,
             {},
             caption: :label_additional_settings,
-            icon: 'icon22 icon-settings2',
+            icon: 'icon2 icon-settings2',
             if: Proc.new { User.current.admin?}
 
   menu.push :ldap_authentication,
@@ -520,23 +520,6 @@ Redmine::MenuManager.map :project_menu do |menu|
             if: Proc.new { |p| p.module_enabled?('stages') },
             icon: 'icon2 icon-etap',
             parent: :stages
-  menu.push :project_strategic_map,
-            {controller: '/project_strategic_map', action: 'index'},
-            icon: 'icon2 icon-organization',
-            caption: :label_strategic_map,
-            param: :project_id,
-            parent: :project_office,
-            if: Proc.new { User.current.admin?||User.current.detect_project_office_coordinator? }
-  menu.push :project_interactive_map,
-            {controller: '/project_interactive_map', action: 'index'},
-            caption: :label_interactive_map,
-            param: :project_id,
-            icon: 'icon2 icon-map',
-            parent: :project_office,
-            #zbd if: Proc.new { User.current.admin?||User.current.detect_project_office_coordinator? }
-            if: Proc.new {
-              User.current.admin?||(User.current.detect_in_global? && User.current.allowed_to_globally?(:view_interactive_map))
-            }
   # )
   # +tan 2019.07.16
   # menu.push :all_plans,
@@ -742,6 +725,25 @@ Redmine::MenuManager.map :project_menu do |menu|
             icon: 'icon2 icon-info1',
             parent: :reports
 
+  #+ knm tan
+  menu.push :project_strategic_map,
+            {controller: '/project_strategic_map', action: 'index'},
+            icon: 'icon2 icon-organization',
+            caption: :label_strategic_map,
+            param: :project_id,
+            parent: :reports# ,
+  # if: Proc.new { User.current.admin? || User.current.detect_project_office_coordinator? }
+  menu.push :project_interactive_map,
+            {controller: '/project_interactive_map', action: 'index'},
+            caption: :label_interactive_map,
+            param: :project_id,
+            icon: 'icon2 icon-map',
+            parent: :reports# ,
+  #zbd if: Proc.new { User.current.admin?||User.current.detect_project_office_coordinator? }
+  # if: Proc.new {
+  #   User.current.admin?||(User.current.detect_in_global? && User.current.allowed_to_globally?(:view_interactive_map))
+  # }
+  # -
 
   menu.push :additional,
             {},
