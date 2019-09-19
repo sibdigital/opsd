a80b173615e4f1b90c3ac7558912da790cbcbb48
griboedok <griboedok@gmail.com>
2019-09-19 10:13:09 +0800

    advanced hierarchy in strategic map

--- app/controllers/strategic_map_controller.rb
+++ app/controllers/strategic_map_controller.rb
@@ -6,15 +6,22 @@ class StrategicMapController < ApplicationController
   end
 
   def get_list
-    list = NationalProject.where(id: params["national_id"]).map { |n| [n.name, n.parent_id, n.id.to_s+n.type, n.type] }
-    federals = NationalProject.where(parent_id: params["national_id"]).map { |f| [f.name, f.parent_id.to_s+"National", f.id.to_s+f.type, f.type] }
     regionals = Array.new
-    regionals = regionals + Project.visible.where(federal_project_id: nil, national_project_id: params["national_id"]).map { |p| [p.name, p.national_project_id.to_s+"National", p.id, "Regional"] }
-    # regionals = regionals + Project.visible.where(federal_project_id: nil, national_project_id: params["national_id"]).map { |p| [p.name, "0Federal", p.id, "Regional"] }
-    federals.each do |federal|
-      regionals = regionals + Project.visible.where(federal_project_id: federal[2]).map { |p| [p.name, p.federal_project_id.to_s+"Federal", p.id, "Regional"] }
-    end
     first_lvl = Array.new
+    federals = Array.new
+    if params["national_id"]=="0"
+      list = [["Проекты Республики Бурятия",nil,"0National","National"]]
+      regionals = regionals + Project.visible.where(federal_project_id: nil, national_project_id: nil).map { |p| [p.name, "0National", p.id, "Regional"] }
+    else
+      list = NationalProject.where(id: params["national_id"]).map { |n| [n.name, n.parent_id, n.id.to_s+n.type, n.type] }
+      federals = NationalProject.where(parent_id: params["national_id"]).map { |f| [f.name, f.parent_id.to_s+"National", f.id.to_s+f.type, f.type] }
+      # regionals = regionals + Project.visible.where(federal_project_id: nil, national_project_id: params["national_id"]).map { |p| [p.name, p.national_project_id.to_s+"National", p.id, "Regional"] }
+      regionals = regionals + Project.visible.where(federal_project_id: nil, national_project_id: params["national_id"]).map { |p| [p.name, "0Federal", p.id, "Regional"] }
+      federals.each do |federal|
+        regionals = regionals + Project.visible.where(federal_project_id: federal[2]).map { |p| [p.name, p.federal_project_id.to_s+"Federal", p.id, "Regional"] }
+      end
+      federals = federals + [["Проекты Республики Бурятия", params["national_id"]+"National", "0Federal", "Federal" ]]
+    end
     regionals.each do |regional|
       WorkPackage.where(project_id: regional[2]).each do |wp|
         if wp.parent == nil
@@ -23,7 +30,6 @@ class StrategicMapController < ApplicationController
       end
       regional[2]=regional[2].to_s+"Regional"
     end
-    # federals = federals + [["Проекты межнационального уровня", params["national_id"]+"National", "0Federal", "Federal" ]]
     list = list + federals + regionals + first_lvl
   render json: list
   end
