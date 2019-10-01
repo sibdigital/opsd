99b27866d36b7ce72dbfed80866ecdb51512a6f4
alxtmf <89021632777@mail.ru>
2019-10-01 09:05:00 +0800

    исправления добавления типов связей между пакетами работ

--- db/migrate/20190920085145_add_commonstart_commonfinish_to_relations.rb
+++ db/migrate/20190920085145_add_commonstart_commonfinish_to_relations.rb
@@ -1,12 +1,13 @@
 class AddCommonstartCommonfinishToRelations < ActiveRecord::Migration[5.2]
   def change
-    add_column :relations, :commonstart, :integer, :default => 0, :null => false
-    add_column :relations, :commonfinish, :integer, :default => 0, :null => false
-    remove_index :relations,
-                 name: 'index_relations_on_type_columns'
-    add_index :relations,
-              %i(from_id to_id hierarchy relates duplicates blocks follows commonstart commonfinish includes requires),
-              name: 'index_relations_on_type_columns',
-              unique: true
+    #add_column :relations, :commonstart, :integer, :default => 0, :null => false
+    #add_column :relations, :commonfinish, :integer, :default => 0, :null => false
+    #
+    # remove_index :relations,
+    #              name: 'index_relations_on_type_columns'
+    # add_index :relations,
+    #           %i(from_id to_id hierarchy relates duplicates blocks follows commonstart commonfinish includes requires),
+    #           name: 'index_relations_on_type_columns',
+    #           unique: true
   end
 end
