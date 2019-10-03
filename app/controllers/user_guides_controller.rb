761efa229bee1570123a8e912360da45215255d7
tmd <tmd979@gmail.com>
2019-10-03 15:38:00 +0800

    Добавлена инструкция пользователя

--- app/controllers/user_guides_controller.rb
+++ app/controllers/user_guides_controller.rb
@@ -1,6 +1,11 @@
 #tmd
 class UserGuidesController < ApplicationController
-  def index
 
+  def download_pdf
+    send_file(
+      "#{Rails.root}/public/Инструкция пользователя.pdf",
+      type: "application/pdf",
+      x_sendfile: true
+    )
   end
 end
