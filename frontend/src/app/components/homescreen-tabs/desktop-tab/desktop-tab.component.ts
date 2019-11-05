79736bc11166df839059d927e07729df21a978c9
bulat <bula3-1992@yandex.ru>
2019-11-05 15:56:30 +0800

    ВЫп список на раб столе

--- frontend/src/app/components/homescreen-tabs/desktop-tab/desktop-tab.component.ts
+++ frontend/src/app/components/homescreen-tabs/desktop-tab/desktop-tab.component.ts
@@ -1,10 +1,16 @@
-import {Component, OnInit} from "@angular/core";
+import {Component, OnInit, ViewChild} from "@angular/core";
 import {HalResourceService} from "core-app/modules/hal/services/hal-resource.service";
 import {PathHelperService} from "core-app/modules/common/path-helper/path-helper.service";
 import {HalResource} from "core-app/modules/hal/resources/hal-resource";
 import {CollectionResource} from "core-app/modules/hal/resources/collection-resource";
 import {WorkPackageResource} from "core-app/modules/hal/resources/work-package-resource";
+import {AngularTrackingHelpers} from "core-components/angular/tracking-functions";
+import {HomescreenBlueTableComponent} from "core-components/homescreen-blue-table/homescreen-blue-table.component";
 
+export interface ValueOption {
+  name:string;
+  $href:string | null;
+}
 
 @Component({
   selector: 'desktop-tab',
@@ -15,6 +21,13 @@ export class DesktopTabComponent implements OnInit {
   protected readonly appBasePath:string;
   public data:any[] = [];
   public problemCount:number = 0;
+
+  public options:any[];
+  private value:{ [attribute:string]:any } | undefined = {};
+  public valueOptions:ValueOption[] = [];
+  public compareByHref = AngularTrackingHelpers.compareByHref;
+
+  @ViewChild(HomescreenBlueTableComponent) blueChild:HomescreenBlueTableComponent;
 
   constructor(
     protected halResourceService:HalResourceService,
@@ -147,6 +160,167 @@ export class DesktopTabComponent implements OnInit {
           this.data[i] = row;
         });
       });
+    this.halResourceService.get<CollectionResource<HalResource>>(this.pathHelper.api.v3.projects.toString())
+      .toPromise()
+      .then((projects:CollectionResource<HalResource>) => {
+        this.valueOptions = projects.elements.map((el:HalResource) => {
+          return {name: el.name, $href: el.id};
+        });
+        this.value = this.valueOptions[0];
+      });
+  }
+
+  public get selectedOption() {
+    const $href = this.value ? this.value.$href : null;
+    return _.find(this.valueOptions, o => o.$href === $href)!;
+  }
+
+  public set selectedOption(val:ValueOption) {
+    let option = _.find(this.valueOptions, o => o.$href === val.$href);
+    this.value = option;
+  }
+
+  public handleUserSubmit() {
+    if (this.selectedOption) {
+      this.blueChild.changeFilter(String(this.selectedOption.$href));
+
+      let from = new Date();
+      let to = new Date();
+      to.setDate(to.getDate() + 14);
+      const filtersGreen = [
+        {
+          status: {
+            operator: 'o',
+            values: []
+          },
+        },
+        {
+          planType: {
+            operator: '~',
+            values: ['execution']
+          }
+        },
+        {
+          type: {
+            operator: '=',
+            values: ['1']
+          }
+        },
+        {
+          dueDate: {
+            operator: '<>d',
+            values: [from.toISOString().slice(0, 10), to.toISOString().slice(0, 10)]
+          }
+        },
+        {
+          project: {
+            operator: '=',
+            values: [String(this.selectedOption.$href)]
+          }
+        }
+      ];
+      this.halResourceService
+        .get<CollectionResource<WorkPackageResource>>(this.pathHelper.api.v3.work_packages_by_role.toString(), {filters: JSON.stringify(filtersGreen)})
+        .toPromise()
+        .then((resources:CollectionResource<WorkPackageResource>) => {
+          resources.elements.map( (el, i) => {
+            let row = this.data[i] ? this.data[i] :{};
+            let id = el.id;
+            let subject = el.subject;
+            let projectId = '';
+            let project = '';
+            if (el.$links.project) {
+              projectId = el.$links.project.href.substr(17);
+              project = el.$links.project.title;
+            }
+            let upcoming_tasks = {id: id, subject: subject, project: project, projectId: projectId};
+            row["upcoming_tasks"] = upcoming_tasks;
+            this.data[i] = row;
+          });
+        });
+      const filtersRed = [
+        {
+          status: {
+            operator: 'o',
+            values: []
+          },
+        },
+        {
+          planType: {
+            operator: '~',
+            values: ['execution']
+          }
+        },
+        {
+          type: {
+            operator: '=',
+            values: ['2']
+          }
+        },
+        {
+          dueDate: {
+            operator: '<t-',
+            values: ['1']
+          }
+        },
+        {
+          project: {
+            operator: '=',
+            values: [String(this.selectedOption.$href)]
+          }
+        }
+      ];
+      this.halResourceService
+        .get<CollectionResource<WorkPackageResource>>(this.pathHelper.api.v3.work_packages_by_role.toString(), {filters: JSON.stringify(filtersRed)})
+        .toPromise()
+        .then((resources:CollectionResource<WorkPackageResource>) => {
+          resources.elements.map( (el, i) => {
+            let row = this.data[i] ? this.data[i] :{};
+            let id = el.id;
+            let subject = el.subject;
+            let projectId = '';
+            let project = '';
+            if (el.$links.project) {
+              projectId = el.$links.project.href.substr(17);
+              project = el.$links.project.title;
+            }
+            let due_milestone = {id: id, subject: subject, project: project, projectId: projectId};
+            row["due_milestone"] = due_milestone;
+            this.data[i] = row;
+          });
+        });
+      this.halResourceService
+        .get<CollectionResource<HalResource>>(this.pathHelper.api.v3.problems.toString(), {"status": "created", "project": this.selectedOption.$href})
+        .toPromise()
+        .then((resources:CollectionResource<HalResource>) => {
+          resources.elements.map( (el, i) => {
+            let row = this.data[i] ? this.data[i] :{};
+            let id = el.id;
+            let risk = el.risk;
+            let wptitle = el.workPackage ? el.workPackage.$links.self.title : '';
+            let wpid = el.workPackage ? el.workPackage.$links.self.href.slice(22) : '';
+            let problems = {id: id, risk: risk, wptitle: wptitle, wpid: wpid};
+            row["problems"] = problems;
+            this.data[i] = row;
+          });
+          this.problemCount = resources.elements ? resources.elements.length : 0;
+        });
+      this.halResourceService
+        .get<HalResource>(this.pathHelper.api.v3.summary_budgets.toString())
+        .toPromise()
+        .then((resources:HalResource) => {
+          resources.source.map( (el:HalResource, i:number) => {
+            let row = this.data[i] ? this.data[i] :{};
+            let id = el.project.id;
+            let name = el.project.name;
+            let spent = el.spent;
+            let total_budget = el.total_budget;
+            let budget = {id: id, name: name, value: total_budget === '0.0' ? 0 : spent / total_budget};
+            row["budget"] = budget;
+            this.data[i] = row;
+          });
+        });
+    }
   }
 
   public getGreenClass(i:number):string {
