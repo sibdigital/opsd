baddaec3428a4466f51bcee7a4738ba2dfbdc2cb
antonb3 <antonb3@yandex.ru>
2019-11-09 17:35:25 +0800

    user_tasks - в процессе разработки фильтра по дате

--- frontend/src/app/modules/grids/widgets/ut-my-tasks/ut-my-tasks.component.ts
+++ frontend/src/app/modules/grids/widgets/ut-my-tasks/ut-my-tasks.component.ts
@@ -5,9 +5,11 @@ import {DocumentResource} from "../../../../../../../modules/documents/frontend/
 import {I18nService} from "core-app/modules/common/i18n/i18n.service";
 import {CollectionResource} from "core-app/modules/hal/resources/collection-resource";
 import {HalResourceService} from "core-app/modules/hal/services/hal-resource.service";
+import {HalResource} from "core-app/modules/hal/resources/hal-resource";
 import {PathHelperService} from "core-app/modules/common/path-helper/path-helper.service";
 import {TimezoneService} from "core-components/datetime/timezone.service";
 import {CurrentUserService} from "core-components/user/current-user.service";
+import {ChartOptions, ChartType, ChartDataSets} from 'chart.js';
 
 @Component({
   templateUrl: './ut-my-tasks.component.html',
@@ -15,29 +17,60 @@ import {CurrentUserService} from "core-components/user/current-user.service";
 
 export class WidgetUtMyTasksComponent extends AbstractWidgetComponent implements OnInit {
   public entries:UserTasksResource[] = [];
+  public dateBegin:string;
+  public dateEnd:string;
+  public dateCheck:boolean = false;
   private entriesLoaded = false;
 
-  constructor(readonly halResource:HalResourceService,
-              readonly pathHelper:PathHelperService,
-              readonly i18n:I18nService,
-              readonly timezone:TimezoneService,
-              readonly currentuser:CurrentUserService) {
+  constructor(readonly i18n:I18nService,
+              readonly timezoneService:TimezoneService,
+              readonly currentuser:CurrentUserService,
+              protected pathHelper:PathHelperService,
+              protected halResourceService:HalResourceService) {
     super(i18n);
   }
 
   ngOnInit() {
-
     let url = `${this.pathHelper.api.v3.apiV3Base}/user_tasks/assigned/${this.currentuser.userId}`;
-
-    this.halResource
-      .get<CollectionResource>(url)
+    this.halResourceService
+      .get<HalResource>(url)
       .toPromise()
-      .then((collection) => {
-        let entriesarray = collection.source as DocumentResource[];
+      .then((resource:HalResource) => {
+        let entriesarray = resource.source as DocumentResource[];
         let entriesarrayforuser = [];
         for (var obj of entriesarray) {
-          if (obj.kind == 'Task'){
-            entriesarrayforuser.push(obj);
+          if (obj.kind == 'Task') {
+              entriesarrayforuser.push(obj);
+          }
+        }
+        this.entries = entriesarrayforuser;
+        this.entriesLoaded = true;
+      });
+  }
+
+  public filterChart() {
+    let url = `${this.pathHelper.api.v3.apiV3Base}/user_tasks/assigned/${this.currentuser.userId}`;
+    this.halResourceService
+      .get<HalResource>(url, this.getParams())
+      .toPromise()
+      .then((resource:HalResource) => {
+        this.dateBegin = this.parser(resource.dateBegin);
+        this.dateEnd =  this.parser(resource.dateEnd);
+        this.dateCheck = (resource.dateCheck);
+        let entriesarray = resource.source as DocumentResource[];
+        let entriesarrayforuser = [];
+        if (resource.dateCheck ==true) {
+          for (var obj of entriesarray) {
+            if (obj.kind == 'Task' && Date.parse(obj.due_date) >= Date.parse(resource.dateBegin) && Date.parse(obj.due_date) <= Date.parse(resource.dateEnd)) {
+              entriesarrayforuser.push(obj);
+            }
+          }
+        }
+        else {
+          for (var obj of entriesarray) {
+            if (obj.kind == 'Task') {
+              entriesarrayforuser.push(obj);
+            }
           }
         }
         this.entries = entriesarrayforuser;
@@ -51,5 +84,30 @@ export class WidgetUtMyTasksComponent extends AbstractWidgetComponent implements
 
   public user_taskCreator(user_task:UserTasksResource) {
     return `${this.pathHelper.appBasePath}/users/${user_task.user_creator_id}`;
+  }
+
+  public formatter(data:any) {
+    if (moment(data, 'YYYY-MM-DD', true).isValid()) {
+      var d = this.timezoneService.parseDate(data);
+      return this.timezoneService.formattedISODate(d);
+    } else {
+      return null;
+    }
+  }
+
+  public parser(data:any) {
+    if (moment(data, 'YYYY-MM-DD', true).isValid()) {
+      return data;
+    } else {
+      return null;
+    }
+  }
+
+  private getParams():any {
+    let params:any = {};
+    params['dateCheck'] = this.dateCheck;
+    params['dateBegin'] = this.dateBegin;
+    params['dateEnd'] = this.dateEnd;
+    return params;
   }
 }
