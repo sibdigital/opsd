b6ee4aee65d83f7ba50f1d36100a2c3acbc59a2a
bulat <bula3-1992@yandex.ru>
2019-11-30 14:07:30 +0800

    424 p4

--- db/migrate/20191130040433_add_is_approved_to_custom_fields.rb
+++ db/migrate/20191130040433_add_is_approved_to_custom_fields.rb
@@ -1,5 +1,5 @@
 class AddIsApprovedToCustomFields < ActiveRecord::Migration[5.2]
   def change
-    add_column :custom_fields, :is_approved, :boolean, default: true
+    add_column :custom_fields, :is_approved, :boolean, default: false
   end
 end
