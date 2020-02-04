4b2cc3988aa178133a1599546a82eb4392b280be
griboedok <griboedok@gmail.com>
2020-02-04 17:03:33 +0800

    Исправлен баг c неправильной проверкой на NULL

--- frontend/src/app/components/homescreen-blue-table/blue-table-types/blue-table-indicator.service.ts
+++ frontend/src/app/components/homescreen-blue-table/blue-table-types/blue-table-indicator.service.ts
@@ -121,7 +121,7 @@ export class BlueTableIndicatorService extends BlueTableService {
                   homescreen_fact_II: target.fact_quarter2_value,
                   homescreen_fact_III: target.fact_quarter3_value,
                   homescreen_fact_IIII: target.fact_quarter4_value,
-                  homescreen_progress: [goal === 0 || fact === 0 ? '0' : (100 * fact / goal).toFixed(1).toString()]
+                  homescreen_progress: [!goal || !fact ? '0' : (100 * fact / goal).toFixed(1).toString()]
                 });
               });
             });
