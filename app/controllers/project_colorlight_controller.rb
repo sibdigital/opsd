3746c17cb0237d75d725da0ab758597d6e258d89
RainbowDwarf <griboedok@gmail.com>
2020-12-02 11:38:55 +0800

    add project clause in colorlight sql request

--- app/controllers/project_colorlight_controller.rb
+++ app/controllers/project_colorlight_controller.rb
@@ -112,6 +112,7 @@ class ProjectColorlightController < ApplicationController
   end
 
   def create
+    find_project
     @items = get_work_packages(params['type'].to_i)
     template_path = File.absolute_path('.') + '/app/reports/templates/blank.xlsx'
     @workbook = RubyXL::Parser.parse(template_path)
@@ -141,28 +142,33 @@ class ProjectColorlightController < ApplicationController
   def get_work_packages(object_type_id)
     sql_command = <<-SQL
       with
-      arb_objects as (
-        select ao.*, e.name as type_name
-        from arbitary_objects as ao
-        inner join enumerations as e on ao.type_id = e.id
-        where type_id = :object_type_id
-      ),
-      work_pack as(
-        select wp.*, ao.name as ao_name, ao.type_name as ao_type_name
-        from work_packages as wp
-        inner join arb_objects as ao on wp.arbitary_object_id = ao.id
-      )
-      select wp.id, wp.subject, wp.contract_id, wp.raion_id, wp.project_id, wp.ao_name, wp.ao_type_name,
-             c.eis_href, c.contract_num, c.contract_date, c.date_begin, c.date_end, c.executor,
-             r.name as raion_name, r.code as raion_code, r.sort_code,
-             p.name as project_name
-      from work_pack as wp
-      left join contracts as c on wp.contract_id = c.id
-      left join raions as r on wp.raion_id = r.id
-      left join projects as p on wp.project_id = p.id
-      order by project_name, sort_code
+            arb_objects as (
+                select ao.*, e.name as type_name
+                from arbitary_objects as ao
+                         inner join enumerations as e on ao.type_id = e.id
+                where type_id = :object_type_id
+            ),
+            work_pack as(
+                select wp.*, ao.name as ao_name, ao.type_name as ao_type_name
+                from (select * 
+                        from work_packages as w 
+                        where w.project_id = :project_id
+                    ) as wp
+                    inner join arb_objects as ao on wp.arbitary_object_id = ao.id
+            )
+        select wp.id, wp.subject, wp.contract_id, wp.raion_id, wp.project_id, wp.ao_name, wp.ao_type_name,
+               c.eis_href, c.contract_num, c.contract_date, c.date_begin, c.date_end, c.executor,
+               r.name as raion_name, r.code as raion_code, r.sort_code,
+               p.name as project_name
+        from work_pack as wp
+                 left join contracts as c on wp.contract_id = c.id
+                 left join raions as r on wp.raion_id = r.id
+                 left join projects as p on wp.project_id = p.id
+        order by project_name, sort_code
     SQL
-    result = ActiveRecord::Base.connection.execute(ActiveRecord::Base.sanitize_sql([sql_command, object_type_id: object_type_id]))
+    result = ActiveRecord::Base.connection.execute(ActiveRecord::Base.sanitize_sql([sql_command,
+                                                                                    object_type_id: object_type_id,
+                                                                                    project_id: @project.id]))
     result.to_a
   end
 end
